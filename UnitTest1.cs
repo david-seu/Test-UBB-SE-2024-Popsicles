799a5af0d9392cf9c36060f4ea8f3ee08dbcc83a
David Seu 916 <david.seu@stud.ubbcluj.ro>
2024-04-25 13:29:41 +0300

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Test_UBB_SE_2024_Popsicles
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
