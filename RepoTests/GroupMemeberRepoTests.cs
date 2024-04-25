59a2d9355e9c4863ef1ccdc04b55bee26965e4d1
914-Laszlo-Marc <marclaszlolucian@gmail.com>
2024-04-25 17:12:55 +0300

    smcvg

--- RepoTests/GroupMemeberRepoTests.cs
+++ RepoTests/GroupMemeberRepoTests.cs
@@ -1,1 +1,86 @@
-﻿using UBB_SE_2024_Popsicles.Repositories;
+﻿//using NUnit.Framework;
+//using System;
+//using System.Linq;
+//using UBB_SE_2024_Popsicles.Models;
+
+//namespace UBB_SE_2024_Popsicles.Tests.Models
+//{
+//    [TestFixture]
+//    public class GroupMemberTests
+//    {
+//        [Test]
+//        public void GetMembership_ExistingMembership_ReturnsMembership()
+//        {
+//            // Arrange
+//            var memberId = Guid.NewGuid();
+//            var groupId = Guid.NewGuid();
+//            var groupMembership = new GroupMembership(groupId, "Role", DateTime.Now,);
+//            var groupMember = new GroupMember(memberId, "username", "password", "email@example.com", "123456789", "description");
+//            groupMember.AddGroupMembership(groupMembership);
+
+//            // Act
+//            var retrievedMembership = groupMember.GetMembership(groupId);
+
+//            // Assert
+//            Assert.Equals(groupMembership, retrievedMembership);
+//        }
+
+//        [Test]
+//        public void GetMembership_NonExistingMembership_ThrowsException()
+//        {
+//            // Arrange
+//            var memberId = Guid.NewGuid();
+//            var groupMember = new GroupMember(memberId, "username", "password", "email@example.com", "123456789", "description");
+
+//            // Act & Assert
+//            Assert.Throws<Exception>(() => groupMember.GetMembership(Guid.NewGuid()));
+//        }
+
+//        [Test]
+//        public void AddGroupMembership_ValidMembership_AddsMembershipToList()
+//        {
+//            // Arrange
+//            var memberId = Guid.NewGuid();
+//            var groupId = Guid.NewGuid();
+//            var groupMembership = new GroupMembership(groupId, "Role", DateTime.Now);
+//            var groupMember = new GroupMember(memberId, "username", "password", "email@example.com", "123456789", "description");
+
+//            // Act
+//            groupMember.AddGroupMembership(groupMembership);
+
+//            // Assert
+//            Assert.Equals(1, groupMember.Memberships.Count);
+//            Assert.That(groupMember.Memberships.Contains(groupMembership));
+//        }
+
+//        [Test]
+//        public void RemoveGroupMembership_ExistingMembership_RemovesMembershipFromList()
+//        {
+//            // Arrange
+//            var memberId = Guid.NewGuid();
+//            var groupId = Guid.NewGuid();
+//            var groupMembership = new GroupMembership(groupId, "Role", DateTime.Now);
+//            var groupMember = new GroupMember(memberId, "username", "password", "email@example.com", "123456789", "description");
+//            groupMember.AddGroupMembership(groupMembership);
+
+//            // Act
+//            groupMember.RemoveGroupMembership(groupMembership.Id);
+
+//            // Assert
+//            Assert.Equals(0, groupMember.Memberships.Count);
+//            Assert.That(groupMember.Memberships.Contains(groupMembership));
+//        }
+
+//        [Test]
+//        public void RemoveGroupMembership_NonExistingMembership_ThrowsException()
+//        {
+//            // Arrange
+//            var memberId = Guid.NewGuid();
+//            var groupMember = new GroupMember(memberId, "username", "password", "email@example.com", "123456789", "description");
+
+//            // Act & Assert
+//            Assert.Throws<Exception>(() => groupMember.RemoveGroupMembership(Guid.NewGuid()));
+//        }
+//    }
+//}
+
--- RepoTests/GroupRepoTests.cs
+++ RepoTests/GroupRepoTests.cs
@@ -1,24 +1,147 @@
-﻿using UBB_SE_2024_Popsicles.Repositories;
+﻿using NUnit.Framework;
+using System;
+using System.Data.SqlClient;
+using UBB_SE_2024_Popsicles.Models;
+using UBB_SE_2024_Popsicles.Repositories;
 
-namespace Test_UBB_SE_2024_Popsicles
+namespace UnitTests
 {
-    public class GroupTests
+    [TestFixture]
+    public class GroupRepositoryTests
     {
+        
+        private GroupRepository repository;
+
         [SetUp]
         public void Setup()
         {
+            string connection = "Server=MARCHOME\\SQLEXPRESS;Database=TestPopsicles;Integrated Security=true;TrustServerCertificate=true;";
+            SqlConnection conn = new SqlConnection(connection);
+            repository = new GroupRepository(conn);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+           
+            repository = null;
         }
 
         [Test]
-        public void Test1()
+        public void AddGroup_Should_Add_New_Group()
         {
-            Assert.Pass();
+            // Arrange
+            Guid Id = Guid.NewGuid();
+            Guid OwnerId = Guid.NewGuid();
+            string Name = "Test Group";
+            string Description = "Test Description";
+            string Icon = "test_icon.png";
+            string Banner = "test_banner.png";
+            int MaxPostsPerHourPerUser = 10;
+            string GroupCode = "TEST";
+            bool IsPublic = true;
+            bool CanMakePostsByDefault = true;
+         
+            Group group = new Group(Id, OwnerId, Name, Description, Icon, Banner, MaxPostsPerHourPerUser,IsPublic, CanMakePostsByDefault, GroupCode);
+            
+            // Act
+            repository.AddGroup(group);
+
+            // Assert
+            Assert.Equals(repository.GetGroups()[0], group);
         }
+
         [Test]
-        public void Test2()
+        public void GetGroupById_Should_Return_Correct_Group()
         {
-            Assert.Pass();
+            // Arrange
+            Guid Id = Guid.NewGuid();
+            Guid OwnerId = Guid.NewGuid();
+            string Name = "Test Group";
+            string Description = "Test Description";
+            string Icon = "test_icon.png";
+            string Banner = "test_banner.png";
+            int MaxPostsPerHourPerUser = 10;
+            string GroupCode = "TEST";
+            bool IsPublic = true;
+            bool CanMakePostsByDefault = true;
+
+            Group group = new Group(Id, OwnerId, Name, Description, Icon, Banner, MaxPostsPerHourPerUser, IsPublic, CanMakePostsByDefault, GroupCode);
+
+            // Act
+            repository.AddGroup(group);
+            // Act
+            Group retrievedGroup = repository.GetGroupById(group.Id);
+
+            // Assert
+            Assert.Equals(group, retrievedGroup);
+        }
+
+        [Test]
+        public void UpdateGroup_Should_Update_Group_Correctly()
+        {
+         
+            // Arrange
+            Guid Id = Guid.NewGuid();
+            Guid OwnerId = Guid.NewGuid();
+            string Name = "Test Group";
+            string Description = "Test Description";
+            string Icon = "test_icon.png";
+            string Banner = "test_banner.png";
+            int MaxPostsPerHourPerUser = 10;
+            string GroupCode = "TEST";
+            bool IsPublic = true;
+            bool CanMakePostsByDefault = true;
+
+            Group group = new Group(Id, OwnerId, Name, Description, Icon, Banner, MaxPostsPerHourPerUser, IsPublic, CanMakePostsByDefault, GroupCode);
+
+            // Act
+            repository.AddGroup(group);
+
+            group.Name = "Updated Group Name";
+            group.Description = "Updated Description";
+            group.Icon = "updated_icon.png";
+            group.Banner = "updated_banner.png";
+            group.MaxPostsPerHourPerUser = 20;
+            group.GroupCode = "UPDATED";
+            group.IsPublic = false;
+            group.CanMakePostsByDefault = false;
+            group.CreatedAt = DateTime.Now.AddDays(-1);
+
+            // Act
+            repository.UpdateGroup(group);
+            Group updatedGroup = repository.GetGroupById(group.Id);
+
+            // Assert
+            Assert.Equals(group, updatedGroup);
+        }
+
+        [Test]
+        public void RemoveGroupById_Should_Remove_Group_Correctly()
+        {
+            // Arrange
+            // Arrange
+            Guid Id = Guid.NewGuid();
+            Guid OwnerId = Guid.NewGuid();
+            string Name = "Test Group";
+            string Description = "Test Description";
+            string Icon = "test_icon.png";
+            string Banner = "test_banner.png";
+            int MaxPostsPerHourPerUser = 10;
+            string GroupCode = "TEST";
+            bool IsPublic = true;
+            bool CanMakePostsByDefault = true;
+
+            Group group = new Group(Id, OwnerId, Name, Description, Icon, Banner, MaxPostsPerHourPerUser, IsPublic, CanMakePostsByDefault, GroupCode);
+
+            // Act
+            repository.AddGroup(group);
+
+            // Act
+            repository.RemoveGroupById(group.Id);
+
+            // Assert
+            Assert.Equals(repository.GetGroups().Count,0);
         }
     }
 }
-//safasfsfwaffaafafafafafaeae
