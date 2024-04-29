17eebcc1e7ef595cff7192987131bc5756b6b864
David Seu 916 <david.seu@stud.ubbcluj.ro>
2024-04-29 13:19:28 +0300

    Update TestGroupService1.cs

--- TestService/TestGroupService1.cs
+++ TestService/TestGroupService1.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 
 namespace Test_UBB_SE_2024_Popsicles.TestService
 {
+
+    [TestFixture]
     internal class TestGroupService1
     {
+
+
+
     }
 }
