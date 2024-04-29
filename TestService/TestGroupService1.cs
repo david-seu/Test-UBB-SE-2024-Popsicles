7fe884eda03b870c3e0bf88028d8de2335835bb8
David Seu 916 <david.seu@stud.ubbcluj.ro>
2024-04-29 13:43:23 +0300

    Update TestGroupService1.cs

--- TestService/TestGroupService1.cs
+++ TestService/TestGroupService1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
+using UBB_SE_2024_Popsicles.Services;
 
 namespace Test_UBB_SE_2024_Popsicles.TestService
 {
@@ -11,8 +12,50 @@ namespace Test_UBB_SE_2024_Popsicles.TestService
     [TestFixture]
     internal class TestGroupService1
     {
+        private Mock<IGroupRepository> groupRepositoryMock;
+        private Mock<IGroupMemberRepository> groupMemberRepositoryMock;
+        private Mock<IGroupMembershipRepository> groupMembershipRepositoryMock;
+        private Mock<IJoinRequestRepository> requestsRepositoryMock;
+        private GroupService groupService;
+        private Guid groupId;
+        private Guid groupMemberId;
 
 
+        [SetUp]
+        public void SetUp()
+        {
+            groupRepositoryMock = new();
+            groupMemberRepositoryMock = new();
+            groupMembershipRepositoryMock = new();
+            requestsRepositoryMock = new();
+
+            groupService = new GroupService(groupRepositoryMock.Object,
+                groupMemberRepositoryMock.Object,
+                groupMembershipRepositoryMock.Object,
+                requestsRepositoryMock.Object);
+
+        }
+
+        public void CreateGroupWithMemberthatBypasesPostageRestriction()
+        {
+            
+        }
+
+        [TearDown] public void TearDown() { }
+
+
+        [Test]
+        public void CreateNewPostOnGroupChat_BypassesPostageRestriction_AddsNewGroupPosts()
+        {
+            //Arrange
+            Guid groupId = this.groupId;
+            Guid groupMemberId = this.groupMemberId;
+            string postContent = "Test post";
+            string postImage = "Test image";
+            
+            //Act
+            groupService.CreateNewPostOnGroupChat(groupId, groupMemberId, postContent, postImage);
+    }
 
     }
 }
