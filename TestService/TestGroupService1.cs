491db7c33f08b6b9b8c03866d50cf2e9338b1202
David Seu 916 <david.seu@stud.ubbcluj.ro>
2024-04-29 14:02:58 +0300

    tests

--- TestService/TestGroupService1.cs
+++ TestService/TestGroupService1.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
+using NUnit.Framework.Legacy;
 using UBB_SE_2024_Popsicles.Services;
+using UBB_SE_2024_Popsicles.Models;
+using UBB_SE_2024_Popsicles.Repositories;
 
 namespace Test_UBB_SE_2024_Popsicles.TestService
 {
-
     [TestFixture]
     internal class TestGroupService1
     {
@@ -20,42 +22,56 @@ namespace Test_UBB_SE_2024_Popsicles.TestService
         private Guid groupId;
         private Guid groupMemberId;
 
-
         [SetUp]
         public void SetUp()
         {
-            groupRepositoryMock = new();
-            groupMemberRepositoryMock = new();
-            groupMembershipRepositoryMock = new();
-            requestsRepositoryMock = new();
+            groupRepositoryMock = new ();
+            groupMemberRepositoryMock = new ();
+            groupMembershipRepositoryMock = new ();
+            requestsRepositoryMock = new ();
 
             groupService = new GroupService(groupRepositoryMock.Object,
                 groupMemberRepositoryMock.Object,
                 groupMembershipRepositoryMock.Object,
                 requestsRepositoryMock.Object);
-
         }
 
-        public void CreateGroupWithMemberthatBypasesPostageRestriction()
+        public Group CreateGroupWithMemberFactory(bool bypassesPostageRestriction = false)
         {
-            
+            groupId = Guid.NewGuid();
+            groupMemberId = Guid.NewGuid();
+            Guid groupMembershipId = Guid.NewGuid();
+
+            GroupMember groupMember = new GroupMember(groupMemberId, "Test User", "Test Password", "Test Email",
+                "Test Phone number", "Test Description");
+
+            GroupMembership groupMembership = new GroupMembership(groupMembershipId, groupMemberId,
+                groupMember.UserName, groupId, "Test Group Member Role", DateTime.Now, false, false, bypassesPostageRestriction);
+
+            Group group = new Group(groupId, groupMemberId, "Test Name", "Test Description", "Test Icon", "Test Banner",
+                5, true, true, "Test Code");
+
+            group.AddMember(groupMembership);
+
+            groupRepositoryMock.Setup(repository => repository.GetGroupById(It.IsAny<Guid>())).Returns(group);
+
+            return group;
         }
-
-        [TearDown] public void TearDown() { }
-
 
         [Test]
         public void CreateNewPostOnGroupChat_BypassesPostageRestriction_AddsNewGroupPosts()
         {
-            //Arrange
+            // Arrange
+            Group group = CreateGroupWithMemberFactory(bypassesPostageRestriction: true);
             Guid groupId = this.groupId;
             Guid groupMemberId = this.groupMemberId;
             string postContent = "Test post";
             string postImage = "Test image";
-            
-            //Act
+            // Act
             groupService.CreateNewPostOnGroupChat(groupId, groupMemberId, postContent, postImage);
-    }
 
+            // Assert
+            ClassicAssert.AreEqual(1, group.ListOfGroupPosts.Count);
+        }
     }
 }
